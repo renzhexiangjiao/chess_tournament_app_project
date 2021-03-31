827685f93229ddb65859c46cde3ba1bd6bf2aa9d
Chuxiang Luo <2572194l@student.gla.ac.uk>
2021-03-31 15:20:45 +0100

    Completed tournament history and uploading populate_chess.py

--- chess/urls.py
+++ chess/urls.py
@@ -5,4 +5,8 @@ app_name = 'chess'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('tournament-history/', views.show_tournament_history, name='history'),
+    path('tournament/<tournament_name>/', 
+         views.show_tournament, name='show_tournament'),
+    path('calendar/', views.show_calendar, name='calendar'),
 ]
--- chess/views.py
+++ chess/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.views import View
+from chess.models import Tournament, Game
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'chess/index.html')
+        
+def show_tournament_history(request):
+    context_dict = {}
+    
+    try:
+        tournaments = Tournament.objects.all()
+        
+        context_dict['tournaments'] = tournaments
+    except Tournament.DoesNotExist:
+        context_dict['tournaments'] = None
+    
+    return render(request, 'chess/tournament_history.html', context=context_dict)
+
+def show_tournament(request, tournament_name):
+    context_dict = {}
+    
+    try:
+        tournament = Tournament.objects.get(name=tournament_name)
+        games = Game.objects.filter(tournament=tournament)
+        
+        context_dict['games'] = games
+        context_dict['tournament'] = tournament
+    except Category.DoesNotExist:
+        context_dict['games'] = None
+        context_dict['tournament'] = None
+    
+    return render(request, 'chess/tournament.html', context=context_dict)
+
+def show_calendar(request):
+    return render(request, 'chess/calendar.html')
