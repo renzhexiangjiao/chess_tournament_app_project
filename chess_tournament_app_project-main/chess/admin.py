8f15efc1b0cc00018b2ea717648974d9567feb92
HANKFU2000 <78972399+HANKFU2000@users.noreply.github.com>
2021-04-06 00:38:41 +0800

    Add files via upload

---
+++ chess_tournament_app_project-main/chess/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from chess.models import Tournament, Game, Move, AccountPage
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date')
+    
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('time', 'player_white', 'player_black', 'tournament')
+
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('moveId', 'square_from', 'square_to')
+
+# Register your models here.
+
+admin.site.register(Tournament, TournamentAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Move, MoveAdmin)
+admin.site.register(AccountPage)
