650cbdbbcd47c0d4d9a030ce1cc7a8a321072f2c
Franciszek Sowul <2482997s@student.gla.ac.uk>
2021-04-06 07:45:31 +0200

    gamerules

--- chess/gamerules.py
+++ chess/gamerules.py
@@ -32,7 +32,7 @@ starting_board_state = {
               11, 11, 11, 11, 11, 11, 11, 11,
               5, 9, 7, 3, 1, 7, 9, 5],
     'turn': 0,
-    'castling': 15,
+    'castling': [1, 1, 1, 1],
     'en_passant': None
 }
 
@@ -40,26 +40,184 @@ def legal_moves(board_state):
     legal_pieces = []
     legal_moves = []
 
+    # select all white / all black pieces
     for pos, piece in enumerate(board_state['board']):
         if piece % 2 == board_state['turn'] and piece!=0:
             legal_pieces.append((pos, piece))
-    
+
+    valid = lambda r, c: c>=0 and c<=7 and r>=0 and r<=7
+    index = lambda r, c: 8*r+c
+
+    # standard move patterns
     for piece in legal_pieces:
+        row = piece[0] // 8
+        col = piece[0] % 8
+
+        # king
         if piece[1] == PieceTypes.BLACK_KING.value or piece[1] == PieceTypes.WHITE_KING.value:
-            pass
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    if valid(row+i, col+j) and (board_state['board'][index(row+i, col+j)] == 0 or (board_state['board'][index(row+i, col+j)] - piece[1]) % 2 == 1):
+                        legal_moves.append((piece[0], index(row+i, col+j)))               
+
+        # queen
+        if piece[1] == PieceTypes.BLACK_QUEEN.value or piece[1] == PieceTypes.WHITE_QUEEN.value:
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    k = 1
+                    while valid(row+i*k, col+j*k) and board_state['board'][index(row+i*k, col+j*k)] == 0:
+                        legal_moves.append((piece[0], index(row+i*k, col+j*k)))
+                        k+=1
+                    if valid(row+i*k, col+i*k) and (board_state['board'][index(row+i*k, col+j*k)] - piece[1]) % 2 == 1:
+                        legal_moves.append((piece[0], index(row+i*k, col+j*k)))
+
+        # rook
+        if piece[1] == PieceTypes.BLACK_ROOK.value or piece[1] == PieceTypes.WHITE_ROOK.value:
+            for i in range(2):
+                for j in range(-1,2):
+                    k = 1
+                    while valid(row+j*k*i, col+j*k*(1-i)) and board_state['board'][index(row+j*k*i, col+j*k*(1-i))] == 0:
+                        legal_moves.append((piece[0], index(row+j*k*i, col+j*k*(1-i))))
+                        k+=1
+                    if valid(row+j*k*i, col+j*k*(1-i)) and (board_state['board'][index(row+j*k*i, col+j*k*(1-i))] - piece[1]) % 2 == 1:
+                        legal_moves.append((piece[0], index(row+j*k*i, col+j*k*(1-i))))
+
+        # bishop
+        if piece[1] == PieceTypes.BLACK_BISHOP.value or piece[1] == PieceTypes.WHITE_BISHOP.value:
+            for i in [-1,1]:
+                for j in [-1,1]:
+                    k = 1
+                    while valid(row+i*k, col+j*k) and board_state['board'][index(row+i*k, col+j*k)] == 0:
+                        legal_moves.append((piece[0], index(row+i*k, col+j*k)))
+                        k+=1
+                    if valid(row+i*k, col+i*k) and (board_state['board'][index(row+i*k, col+j*k)] - piece[1]) % 2 == 1:
+                        legal_moves.append((piece[0], index(row+i*k, col+j*k)))
+
+        # knight
+        if piece[1] == PieceTypes.BLACK_KNIGHT.value or piece[1] == PieceTypes.WHITE_KNIGHT.value:
+            for i in [-2, -1, 1, 2]:
+                for j in [-1, 1]:
+                    if valid(row+i, col+j*(3-abs(i))) and (board_state['board'][index(row+i, col+j*(3-abs(i)))] == 0 or (board_state['board'][index(row+i, col+j*(3-abs(i)))] - piece[1]) % 2 == 1):
+                        legal_moves.append((piece[0], index(row+i, col+j*(3-abs(i))))) 
+
+        # white pawn
+        if piece[1] == PieceTypes.WHITE_PAWN.value:
+            if row == 1 and board_state['board'][index(row+1, col)] == 0 and board_state['board'][index(row+2, col)] == 0:
+                legal_moves.append((piece[0], index(row+2, col)))
+            if valid(row+1, col) and board_state['board'][index(row+1, col)] == 0:
+                legal_moves.append((piece[0], index(row+1, col)))
+            if valid(row+1, col+1) and board_state['board'][index(row+1, col+1)] != 0 and (board_state['board'][index(row+1, col+1)] - piece[1]) % 2 == 1:
+                legal_moves.append((piece[0], index(row+1, col+1)))
+            if valid(row+1, col-1) and board_state['board'][index(row+1, col-1)] != 0 and (board_state['board'][index(row+1, col-1)] - piece[1]) % 2 == 1:
+                legal_moves.append((piece[0], index(row+1, col-1)))
+
+        # black pawn
+        if piece[1] == PieceTypes.BLACK_PAWN.value:
+            if row == 6 and board_state['board'][index(row-1, col)] == 0 and board_state['board'][index(row-2, col)] == 0:
+                legal_moves.append((piece[0], index(row-2, col)))
+            if valid(row-1, col) and board_state['board'][index(row-1, col)] == 0:
+                legal_moves.append((piece[0], index(row-1, col)))
+            if valid(row-1, col+1) and board_state['board'][index(row-1, col+1)] != 0 and (board_state['board'][index(row-1, col+1)] - piece[1]) % 2 == 1:
+                legal_moves.append((piece[0], index(row-1, col+1)))
+            if valid(row-1, col-1) and board_state['board'][index(row-1, col-1)] != 0 and (board_state['board'][index(row-1, col-1)] - piece[1]) % 2 == 1:
+                legal_moves.append((piece[0], index(row-1, col-1)))
+
+    legal_moves_str = []
+
+    # move by white can't create a check for white, move for black can't create a check for black
+    for move in legal_moves:
+        piece_captured = board_state['board'][move[1]]
+
+        # simulate move
+        board_state['board'][move[1]] = board_state['board'][move[0]]
+        board_state['board'][move[0]] = 0
+
+        # check?
+        if board_state['turn']:
+            if not check_black(board_state):
+                legal_moves_str.append(chr(ord('a') + move[0]%8) + str(move[0]//8+1) + chr(ord('a') + move[1]%8) + str(move[1]//8+1))
+        else:
+            if not check_white(board_state):
+                legal_moves_str.append(chr(ord('a') + move[0]%8) + str(move[0]//8+1) + chr(ord('a') + move[1]%8) + str(move[1]//8+1))
+
+        # move the piece back
+        board_state['board'][move[0]] = board_state['board'][move[1]]
+        board_state['board'][move[1]] = piece_captured
+    
+    return legal_moves_str
 
 
 def make_move(board_state, sq_from, sq_to):
-    pass
+    index_from = 8*(int(sq_from[1])) + ord(sq_from[0]) - ord('a')
+    index_to = 8*(int(sq_to[1])) + ord(sq_to[0]) - ord('a')
+    
+    piece_moved = board_state['board'][index_from]
+    piece_captured = board_state['board'][index_to]
+
+    # castling move rook
+    if piece_moved == PieceTypes.WHITE_KING.value and sq_from == 'e1' and sq_to == 'g1':
+        board_state['board'][5] = PieceTypes.WHITE_ROOK.value
+        board_state['board'][7] = 0
+    if piece_moved == PieceTypes.WHITE_KING.value and sq_from == 'e1' and sq_to == 'c1':
+        board_state['board'][3] = PieceTypes.WHITE_ROOK.value
+        board_state['board'][0] = 0
+    if piece_moved == PieceTypes.WHITE_KING.value and sq_from == 'e8' and sq_to == 'g8':
+        board_state['board'][61] = PieceTypes.WHITE_ROOK.value
+        board_state['board'][63] = 0
+    if piece_moved == PieceTypes.WHITE_KING.value and sq_from == 'e8' and sq_to == 'c8':
+        board_state['board'][59] = PieceTypes.WHITE_ROOK.value
+        board_state['board'][56] = 0
+
+    # castling
+    if piece_moved == PieceTypes.WHITE_KING.value:
+        board_state['castling'][0], board_state['castling'][1] = 0, 0
+    if piece_moved == PieceTypes.WHITE_ROOK.value and sq_from == 'a1':
+        board_state['castling'][0] = 0
+    if piece_moved == PieceTypes.WHITE_ROOK.value and sq_from == 'a8':
+        board_state['castling'][1] = 0
+    if piece_moved == PieceTypes.BLACK_KING.value:
+        board_state['castling'][2], board_state['castling'][3] = 0, 0
+    if piece_moved == PieceTypes.BLACK_ROOK.value and sq_from == 'h1':
+        board_state['castling'][2] = 0
+    if piece_moved == PieceTypes.BLACK_ROOK.value and sq_from == 'h8':
+        board_state['castling'][3] = 0
+
+    # en passant remove pawn
+    if piece_moved == PieceTypes.WHITE_PAWN.value and sq_to == board_state['en_passant']:
+        board_state['board'][index_to - 8] = 0
+    if piece_moved == PieceTypes.BLACK_PAWN.value and sq_to == board_state['en_passant']:
+        board_state['board'][index_to + 8] = 0
+
+    # en passant
+    if piece_moved == PieceTypes.WHITE_PAWN.value and sq_from[1] == '2' and sq_to[1] == '4':
+        board_state['en_passant'] = sq_from[0] + '3'
+    elif piece_moved == PieceTypes.BLACK_PAWN.value and sq_from[1] == '7' and sq_to[1] == '5':
+        board_state['en_passant'] = sq_from[0] + '6'
+    else:
+        board_state['en_passant'] = None
+
+    # change turn
+    board_state['turn'] = 1 - board_state['turn']
+
+    # move piece on the board
+    board_state['board'][index_to] = board_state['board'][index_from]
+    board_state['board'][index_from] = 0
+
+    # promotion
+    if piece_moved == PieceTypes.WHITE_PAWN.value and sq_to[1] == '8':
+        board_state['board'][index_to] = PieceTypes.WHITE_QUEEN.value
+    if piece_moved == PieceTypes.BLACK_PAWN.value and sq_to[1] == '1':
+        board_state['board'][index_to] = PieceTypes.BLACK_QUEEN.value
+        
 
 def check_white(board_state):
-    pass
+    return False
 
 def mate_white(board_state):
     return check_white(board_state) and len(legal_moves(board_state))==0
 
-def check_white(board_state):
-    pass
+def check_black(board_state):
+    return False
 
 def mate_black(board_state):
     return check_black(board_state) and len(legal_moves(board_state))==0
--- chess/views.py
+++ chess/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from . import gamerules
 
 class IndexView(View):
     def get(self, request):
@@ -14,7 +15,7 @@ class PlayView(View):
 class MoveListView(View):
     def get(self, request):
         print(request.GET['move'])
-        context_dict = { 'options' : ['a2a3', 'a2a4', 'b2b3', 'b2b4', 'c2c3', 'c2c4', 'd2d3', 'd2d4', 'e2e3', 'e2e4', 'f2f3', 'f2f4', 'g2g3', 'g2g4', 'h2h3', 'h2h4', 'b1a3', 'b1c3', 'g1f3', 'g1h3']}
+        context_dict = { 'options' : gamerules.legal_moves(gamerules.starting_board_state) }
         return render(request, 'chess/movelist.html', context_dict)
 
 class MoveUpdateView(View):
